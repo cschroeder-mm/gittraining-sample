ba4d6250820be7035a5cb663dd2ce50ae4c972ef
Christian Schroeder <cschroeder@meyle-mueller.de>
2013-04-08 14:08:40 +0200

    Working on mutiple
    Just normal multiple

--- org.eclipse.example.calc/src/org/eclipse/example/calc/internal/Calculator.java
+++ org.eclipse.example.calc/src/org/eclipse/example/calc/internal/Calculator.java
@@ -15,6 +15,7 @@ import org.eclipse.example.calc.Operations;
 import org.eclipse.example.calc.UnaryOperation;
 import org.eclipse.example.calc.internal.operations.Equals;
 import org.eclipse.example.calc.internal.operations.Minus;
+import org.eclipse.example.calc.internal.operations.Multiple;
 import org.eclipse.example.calc.internal.operations.Plus;
 import org.eclipse.example.calc.internal.operations.Square;
 
@@ -40,6 +41,7 @@ public class Calculator {
 		new Minus();
 		new Plus();
 		new Square();
+		new Multiple();
 	}
 
 	private void calculate(String cmdName) {
