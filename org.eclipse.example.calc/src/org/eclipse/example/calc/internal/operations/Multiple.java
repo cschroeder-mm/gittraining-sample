23022bb295af1c0ce013b90bf684182bfc1de739
Christian Schroeder <cschroeder@meyle-mueller.de>
2013-04-08 14:08:40 +0200

    Working on mutiple
    Now with even bigger numbers with to multiple

---
+++ org.eclipse.example.calc/src/org/eclipse/example/calc/internal/operations/Multiple.java
@@ -0,0 +1,22 @@
+package org.eclipse.example.calc.internal.operations;
+
+import org.eclipse.example.calc.BinaryOperation;
+
+public class Multiple extends AbstractOperation implements BinaryOperation
+{
+
+	@Override
+	public String getName()
+	{
+		// TODO Auto-generated method stub
+		return "*";
+	}
+
+	@Override
+	public float perform(float arg1, float arg2)
+	{
+		// TODO Auto-generated method stub
+		return arg1 * arg2 *2;
+	}
+
+}
